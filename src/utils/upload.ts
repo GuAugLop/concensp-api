c526635f782d400ab9558769152d67a167170822
Gustavo Lopes <gulopes.augusto@gmail.com>
2022-10-31 23:09:34 -0300

    fix

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -1,8 +1,9 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../public/arquivos");
+    cb(null, path.resolve(__dirname, "..", "..", "public", "arquivos"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
